3c5b7c91a69ace75370e80660842ea656070becf
Talha Irshad <tirshad94@gmail.com>
2021-06-08 03:33:12 +0500

    Added redux to product screen

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,26 @@
+
 import express from 'express';
 import data from './data.js';
 
 const app = express();
 
-app.get('/api/products', (req, res)=>{
+app.get('/api/products/:id', (req, res) => {
+    const product = data.product.find(x => x._id === req.params.id)
+    if (product) {
+        res.send(product);
+    } else {
+        res.status(404).send({ message: 'Product not found' })
+    }
+
+})
+
+app.get('/api/products', (req, res) => {
     res.send(data.product)
 })
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Server is ready');
 });
 const port = process.env.PORT || 5000
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Server at http://localhost:${port}`)
 })
