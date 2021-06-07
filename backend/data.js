abf38e5c1f5238d3bb181ed0ed26d1bcb4ca30ed
Talha Irshad <tirshad94@gmail.com>
2021-06-07 15:55:55 +0500

    added redux

---
+++ backend/data.js
@@ -0,0 +1,175 @@
+const data={
+    product:[
+        {
+            _id:'1',
+            name:'Plain White Shirt',
+            category:'Shirts',
+            image:'images/p1.jpg',
+            price:1000,
+            brand:'Nike',
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'2',
+            name:'Plain Black Shirt',
+            category:'Shirts',
+            image:'images/p2.jpg',
+            price:1200,
+            brand:'Nike',
+            countInStock:10,
+            rating:3.5,
+            numReviews:20,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'3',
+            name:'Plain Gray Shirt',
+            category:'Shirts',
+            image:'images/p3.jpg',
+            price:1400,
+            brand:'Nike',
+            countInStock:10,
+            rating:4,
+            numReviews:40,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'4',
+            name:'Plain Adidas black trousers',
+            category:'trousers',
+            image:'images/p4.jpg',
+            price:1100,
+            brand:'Adidas',
+            countInStock:10,
+            rating:4.5,
+            numReviews:30,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'5',
+            name:'Plain Adidas gray trousers',
+            category:'trousers',
+            image:'images/p5.jpg',
+            price:1300,
+            brand:'Adidas',
+            countInStock:10,
+            rating:5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'6',
+            name:'Plain Adidas white trousers',
+            category:'trousers',
+            image:'images/p6.jpg',
+            price:1400,
+            brand:'Adidas',
+            countInStock:10,
+            rating:3.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'7',
+            name:'Plain Adidas trousers',
+            category:'trousers',
+            image:'images/p1.jpg',
+            price:1400,
+            brand:'Nike',
+            countInStock:10,
+            rating:4.5,
+            numReviews:30,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'8',
+            name:'Plain Adidas white trousers',
+            category:'trousers',
+            image:'images/p2.jpg',
+            price:1400,
+            brand:'Adidas',
+            countInStock:10,
+            rating:2.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'9',
+            name:'Plain Adidas white trousers',
+            category:'trousers',
+            image:'images/p3.jpg',
+            price:1400,
+            brand:'Nike',
+            countInStock:10,
+            rating:4,
+            numReviews:50,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'10',
+            name:'Plain Adidas white trousers',
+            category:'trousers',
+            image:'images/p4.jpg',
+            price:1400,
+            brand:'Nike',
+            countInStock:10,
+            rating:4,
+            numReviews:30,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'11',
+            name:'Plain Adidas black trousers',
+            category:'trousers',
+            image:'images/p5.jpg',
+            price:1400,
+            brand:'Adidas',
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'12',
+            name:'Trousers',
+            category:'trousers',
+            image:'images/p6.jpg',
+            price:1400,
+            brand:'Adidas',
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'13',
+            name:'Plain white trousers',
+            category:'trousers',
+            image:'images/p3.jpg',
+            price:1400,
+            brand:'Adidas',
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+        {
+            _id:'14',
+            name:'Plain Adidas trousers',
+            category:'trousers',
+            image:'images/p2.jpg',
+            price:1400,
+            brand:'Adidas',
+            countInStock:10,
+            rating:4.5,
+            numReviews:10,
+            description:'High Quality Shirt from Nike'
+        },
+
+    ]
+}
+
+export default data;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+
 import Product from '../components/Product';
-import data from '../data';
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
+import { useDispatch, useSelector } from 'react-redux'
+import { listProduct } from '../actions/productActions';
+// import data from '../data';
 
 export default function HomeScreen() {
+    const dispatch = useDispatch();
+    const productList = useSelector((state) => state.productList);
+    const { loading, error, product } = productList;
+
+    useEffect(() => {
+        dispatch(listProduct());
+        // const fetchData = async () => {
+        //     try {
+        //         setLoading(true);
+        //         const { data } = await axios.get('/api/products')
+        //         setLoading(false);
+        //         setProducts(data);
+        //     } catch (err) {
+        //         setError(err.message);
+        //         setLoading(false)
+        //     }
+        // };
+        // fetchData();
+    }, [])
     return (
         <div>
-            <div className="row center">
-                {
-                    data.product.map(product => (
-                        <Product key={product._id} product={product}></Product>
-                    ))
-                }
+            {loading ? (<LoadingBox></LoadingBox>)
+                :
+                error ? (<MessageBox variant="danger">{error}</MessageBox>)
+                    :
+                    (<div className="row center">
+                        {
+                            product.map(product => (
+                                <Product key={product._id} product={product}></Product>
+                            ))
+                        }
 
-            </div>
+                    </div>
+                    )}
         </div>
     )
 }
